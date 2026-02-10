5fff34f514b614ba37780a5b994681da2ed61304
Pranav Sonawane <pranavsonawane2907@gmail.com>
2026-02-10 23:37:45 +0530

    ( File Handling ) Create File using Separate Function

---
+++ FileHandling2.c
@@ -0,0 +1,37 @@
+/*
+	Description :-( File Handling ) Create File using Seprate Function
+	Author :- Pranav R Sonawane
+*/ 
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h> // File Control
+
+int CreateFile(char Name[])
+{
+	int fd = 0;
+	fd = creat(Name,0777);
+	return fd;
+}
+
+int main()
+{
+	char Fname[20];
+	int fd = 0; // File Discriptor
+	
+	printf("Enter the file name that you want to create : ");
+	scanf("%s",Fname);
+	
+	fd = CreateFile(Fname);
+	
+	if(fd == -1)
+	{
+		printf("Unable to create file \n");
+	}
+	else
+	{
+		printf("File is successfully created with FD %d\n",fd);
+	}
+	
+	return 0;
+}
