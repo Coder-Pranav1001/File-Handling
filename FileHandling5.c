3f1032bc6710b819e64d24f0b5587fc32e86a776
Pranav Sonawane <pranavsonawane2907@gmail.com>
2026-02-10 23:38:54 +0530

    ( File Handling ) Write File

---
+++ FileHandling5.c
@@ -0,0 +1,36 @@
+/*
+	Description :-( File Handling ) Write File 
+	Author :- Pranav R Sonawane
+*/ 
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h> // File Control
+
+// O_RDONLY		Open for reading
+// O_WRONLY		Open for writing
+// O_RDWR		Open for Reading and Writing
+
+int main()
+{
+	char Fname[20];
+	int fd = 0; // File Discriptor
+	char Data[] = "MARVELLOUS";
+	
+	printf("Enter the file name that you want to open : ");
+	scanf("%s",Fname);
+	
+	fd = open(Fname,O_RDWR); 
+	
+	if(fd == -1)
+	{
+		printf("Unable to open file\n");
+	}
+	else
+	{
+		printf("File is succesfully opened with FD %d\n",fd);
+		write(fd,Data,10);
+	}
+	
+	return 0;
+}
