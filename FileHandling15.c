d575f9ae7b25fa3f2eae46bd04335a5a7ea994af
Pranav Sonawane <pranavsonawane2907@gmail.com>
2026-02-11 11:42:15 +0530

    Accept file name from user Read that file and count Capital Case Character

---
+++ FileHandling15.c
@@ -0,0 +1,47 @@
+/*
+	Description :- Accept file name from user Read that file and count Capital Case Character
+	Author :- Pranav R Sonawane
+*/  
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h> 
+#include<string.h>
+
+int main()
+{
+	char Fname[20];
+	int fd = 0; 
+	int Length = 0;
+	char Data[100];
+	int Count = 0;
+	int iCnt = 0;
+	
+	printf("Enter the file name that you want to open : ");
+	scanf("%s",Fname);
+	
+	fd = open(Fname, O_RDONLY);
+	
+	if(fd == -1)
+	{
+		printf("Unable to open file 'n");
+		return -1;
+	}
+	
+	while((Length = read(fd,Data,sizeof(Data))) != 0) 
+	{
+		for(iCnt = 0; iCnt < Length; iCnt++)
+		{
+			if(Data[iCnt] >= 'A' && Data[iCnt] <= 'Z')
+			{
+				Count++;
+			}
+		}
+	}
+	
+	printf("Number of Capital Case letters are : %d\n",Count);
+	
+	close(fd);
+	
+	return 0;
+}
