7c9bcaecb289e13b38f4aa2cccc70e3c5a8b25f0
Pranav Sonawane <pranavsonawane2907@gmail.com>
2026-02-21 22:54:55 +0530

    Accept file name from user and read Data from that file using string format

---
+++ File5.java
@@ -0,0 +1,37 @@
+/*
+	Description :- Accept file name from user and read Data from that file using string format
+	Author :- Pranav R Sonawane
+*/
+
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+
+class File5
+{
+	public static void main(String arg[]) throws Exception 
+	{
+		Scanner sobj = new Scanner(System.in);
+		
+		System.out.println("Enter file name : ");
+		String FileName = sobj.nextLine();
+		
+		File fobj = new File(FileName);
+		
+		if(fobj.exists())
+		{
+			BufferedReader bobj = new BufferedReader(new FileReader(fobj));
+			
+			String data;
+			
+			while((data = bobj.readLine()) != null)
+			{
+				System.out.println(data);
+			}
+		}
+		else
+		{
+			System.out.println("There is no such file");
+		}
+	} 
+}
