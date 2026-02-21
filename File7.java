969d5192417581bfda8599f3d0bbe68226f4d9d2
Pranav Sonawane <pranavsonawane2907@gmail.com>
2026-02-21 22:55:54 +0530

    Accept file name from user and read Data from that file using byte format

---
+++ File7.java
@@ -0,0 +1,38 @@
+/*
+	Description :- Accept file name from user and read Data from that file using byte format
+	Author :- Pranav R Sonawane
+*/
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+class File7
+{
+	public static void main(String arg[]) throws Exception 
+	{
+		Scanner sobj = new Scanner(System.in);
+		
+		System.out.println("Enter file name : ");
+		String FileName = sobj.nextLine();
+		
+		File fobj = new File(FileName);
+		
+		if(fobj.exists())
+		{
+			FileInputStream fiobj = new FileInputStream(fobj);
+			
+			byte Buffer[] = new byte[100];
+			int ret = 0;
+			while((ret = fiobj.read(Buffer)) != -1)
+			{
+				String str = new String(Buffer,StandardCharsets.UTF_8);
+				System.out.println(str);
+			}
+		}
+		else
+		{
+			System.out.println("There is no such file");
+		}
+	} 
+}
