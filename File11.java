4db8f1069a32efc9999ce35f5834124b19dfc344
Pranav Sonawane <pranavsonawane2907@gmail.com>
2026-02-22 21:32:40 +0530

    Accept folder name from user and that folder is exists or not

---
+++ File11.java
@@ -0,0 +1,35 @@
+/*
+	Description :- Accept folder name from user and that folder is exists or not
+	Author :- Pranav R Sonawane
+*/
+import java.io.*;
+import java.util.*;
+
+class File11
+{
+	public static void main(String a[])
+	{
+		Scanner sobj = new Scanner(System.in);
+	
+		try
+		{
+			System.out.println("Enter Folder Name : ");
+			String FolderName = sobj.nextLine();
+			
+			File fobj = new File(FolderName);
+			
+			if(fobj.exists())
+			{
+				
+			}
+			else
+			{
+				System.out.println("There is no such folder...");
+			}
+		}
+		catch(Exception obj)
+		{
+			System.out.println("Exception occured : "+obj);
+		}
+	}
+}
